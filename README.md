9658804bafacad7494c7b94b0aafd6770438d053
Michiel85 <68066270+Michiel85@users.noreply.github.com>
2024-01-08 15:12:14 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # HelloID-Task-SA-Source-ActiveDirectory-AccountGetGroupMemberships
+
+## Prerequisites
+
+- [ ] The HelloID SA on-premises agent installed
+- [ ] The ActiveDirectory module is installed on the server where the HelloID SA on-premises agent is running.
+- [ ] Data source input parameter (required parameter) named `selectedUser`. The linked form-element should at least return a model containing the `UserPrincipalName` property
+
+
+## Description
+
+This code snippet executes the following tasks:
+
+1. Define `$userPrincipalName` based on the `selectedUser` data source input `$dataSource.selectedUser.UserPrincipalName`
+2. Imports the ActiveDirectory module.
+3. Retrieve the AD user using the `Get-ADUser` cmdlet.
+4. Get the AD group memberships of the AD user using the `Get-ADPrincipalGroupMembership` cmdlet.
+5. Remove the `Domain Users` group from the list
+6. Return a hash table for each group membership using the `Write-Output` cmdlet.
+
+> To view an example of the data source output, please refer to the JSON code pasted below and select the `Interpreted as JSON` option in HelloID
+
+```json
+{
+    "UserPrincipalName": "a.acevedo@domain.local"
+}
+```
---
+++ action.ps1
@@ -0,0 +1,32 @@
+try {
+    $userPrincipalName = $dataSource.selectedUser.UserPrincipalName
+    Write-Information "Searching AD user [$userPrincipalName]"
+
+    Import-Module ActiveDirectory -ErrorAction Stop
+    
+    $adUser = Get-ADuser -Filter { UserPrincipalName -eq $userPrincipalName } 
+
+    if([String]::IsNullOrEmpty($adUser) -eq $true) {
+        $msg = "Could not find AD user [$userPrincipalName]"
+        Write-Error $msg
+    } else {
+        Write-Information "Found AD user [$userPrincipalName]"
+
+        $groups = Get-ADPrincipalGroupMembership -identity $adUser | Select-Object name, sid | Sort-Object name
+        $groups = $groups | Where-Object {$_.Name -ne "Domain Users"}
+        $resultCount = @($groups).Count
+        
+        Write-Information "Group membership count: $resultCount"
+        
+        if($resultCount -gt 0) {
+            foreach($group in $groups)
+            {
+                $returnObject = @{name="$($group.name)"; sid = [string]"$($group.sid)"}
+                Write-Output $returnObject
+            }
+        }
+    }
+} catch {
+    $msg = "Error getting group memberships [$userPrincipalName]. Error: $($_.Exception.Message)"
+    Write-Error $msg
+}
